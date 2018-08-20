bc47b63881655ac9ac9f42b5b855119c587e5ee1
Kle1ton <pauloricardo123425@gmail.com>
2018-08-20 20:15:32 -0300

    Exemplo

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-git clone git remote add origin https://github.com/daleonrd/tpi-ii.git
+git clone git remote add origin https://github.com/kle1ton/tpi-ii
 git config --global user.email "pauloricardo123425@gmail.com"
 git config --global user.name "Kle1ton"
 cd tpi-ii
